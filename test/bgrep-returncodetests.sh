0ab375b52c51f9d50bf6a784634854878c8a6855
rsharo <rsharo@users.noreply.github.com>
2017-01-22 23:24:36 -0500

    Finished out bgrep-returncodetests.sh.
    Code is probably ready for next release.

--- test/bgrep-returncodetests.sh
+++ test/bgrep-returncodetests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 BGREP=../src/bgrep
 
 
@@ -18,6 +16,82 @@ function return_code_test() {
 		echo "Input was: ${input}"
 		return 1
 	fi
+	return 0
+}
+
+function file_not_found_test() {
+	local input="$1"
+	local pattern="$2"
+	local expected=2
+
+	echo -en "${input}" | ${BGREP} -q -x "${pattern}" - foo 2>/dev/null
+	local result="$?"
+	if [[ "${result}" -ne "${expected}" ]] ; then
+		echo "${FUNCNAME[0]}: Test FAILED."
+		echo "Pattern ${pattern} should have returned code ${expected} due to file error (was ${result})"
+		echo "Input was: ${input}"
+		return 1
+	fi
+	return 0
+}
+
+function multiple_result_test() {
+	local pattern='"foo"'
+	local expected=0
+
+	mkdir -p /tmp/bgrep_test$$/x/y/z
+	echo "foo" > /tmp/bgrep_test$$/x/file1.txt
+	echo "bar" > /tmp/bgrep_test$$/x/file2.txt
+
+	# searchfile() returns 0, then returns 1
+	pattern='"foo"'
+	expected=0
+	${BGREP} -r -q -x "${pattern}" /tmp/bgrep_test$$
+	local result="$?"
+	if [[ "${result}" -ne "${expected}" ]] ; then
+		echo "${FUNCNAME[0]}: Test FAILED."
+		echo "Pattern ${pattern} should have returned code ${expected} (was ${result})"
+		echo "Input was: ${input}"
+		return 1
+	fi
+
+	# searchfile() returns 1, then returns 0
+	pattern='"bar"'
+	expected=0
+	${BGREP} -r -q -x "${pattern}" /tmp/bgrep_test$$
+	local result="$?"
+	if [[ "${result}" -ne "${expected}" ]] ; then
+		echo "${FUNCNAME[0]}: Test FAILED."
+		echo "Pattern ${pattern} should have returned code ${expected} (was ${result})"
+		echo "Input was: ${input}"
+		return 1
+	fi
+
+	# searchfile() returns 1 twice (sanity check)
+	pattern='"baz"'
+	expected=1
+	${BGREP} -r -q -x "${pattern}" /tmp/bgrep_test$$
+	local result="$?"
+	if [[ "${result}" -ne "${expected}" ]] ; then
+		echo "${FUNCNAME[0]}: Test FAILED."
+		echo "Pattern ${pattern} should have returned code ${expected} (was ${result})"
+		echo "Input was: ${input}"
+		return 1
+	fi
+
+	# Never even get to searchfile() (sanity check, no -r flag)
+	pattern='"foo"'
+	expected=2
+	${BGREP} -q -x "${pattern}" /tmp/bgrep_test$$ 2>/dev/null
+	local result="$?"
+	if [[ "${result}" -ne "${expected}" ]] ; then
+		echo "${FUNCNAME[0]}: Test FAILED."
+		echo "Pattern ${pattern} should have returned code ${expected} (was ${result})"
+		echo "Input was: ${input}"
+		return 1
+	fi
+
+	rm -r /tmp/bgrep_test$$
 	return 0
 }
 
@@ -76,6 +150,14 @@ do
 	return_code_test "${code_1_inputs[$p]}" "${p}" 1 || failcount=$((failcount+1))
 done
 
+# Verify "error" result overrides "no match"
+file_not_found_test "" '??' || failcount=$((failcount+1))
+
+# Verify "error" result overrides "match"
+file_not_found_test "a" '??' || failcount=$((failcount+1))
+
+# Verify the right thing happens when some files match and others don't
+multiple_result_test || failcount=$((failcount+1))
 
 if [[ ${failcount} -eq 0 ]] ; then
 	echo ALL TESTS PASSED.
